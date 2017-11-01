15274e2832f4c124b41308b0eb84ae93d91747c5
MF <notsure@compuserv.com>
2017-10-31 22:56:12 -0500

    comments

--- app/src/main/java/com/example/hl4350hb/randomteamselect/MainActivity.java
+++ app/src/main/java/com/example/hl4350hb/randomteamselect/MainActivity.java
@@ -22,21 +22,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // References widgets.
         mTeam1Text = (EditText) findViewById(R.id.team1name);
         mTeam2Text = (EditText) findViewById(R.id.team2name);
         mSelectButton = (Button) findViewById(R.id.selectrandombutton);
         mResultTextView = (TextView) findViewById(R.id.resulttextview);
 
+        // Creates a click event for button.
         mSelectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Grabs values from EditText widgets.
                 String team1Value = mTeam1Text.getText().toString();
                 String team2Value = mTeam2Text.getText().toString();
 
+                // Compares values to see if any fields were left blank.
                 if (team1Value == null || team2Value == null || team1Value.equals("") || team2Value.equals("")) {
+                    // Displays Toast if a field is empty.
                     Toast.makeText(MainActivity.this, "You need two teams entered!", Toast.LENGTH_LONG).show();
                 }
                 else {
+                    // Randomly selects a team and displays the results.
                     int random = new Random().nextInt(2);
                     switch (random) {
                         case 0:
